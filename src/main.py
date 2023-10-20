cd62796e4123cf81a278e12bbc179130509c0503
davydaaa <andriy2davyd@gmail.com>
2023-10-20 12:12:39 +0300

    create

---
+++ src/main.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def sum_of_depths(root: TreeNode) -> int:
+    if root is None:
+        return 0
+
+    stack = [(root, 0)]
+    total_depth = 0
+
+    while stack:
+        node, depth = stack.pop()
+        total_depth += depth
+
+        if node.left:
+            stack.append((node.left, depth + 1))
+        if node.right:
+            stack.append((node.right, depth + 1))
+
+    return total_depth
+
---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+import unittest
+from src.main import sum_of_depths, TreeNode
+
+class TestSumOfDepths(unittest.TestCase):
+    def test1(self):
+        root = TreeNode(1)
+        self.assertEqual(sum_of_depths(root), 0)
+
+    def test2(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.left = TreeNode(4)
+        root.left.right = TreeNode(5)
+        self.assertEqual(sum_of_depths(root), 6)
+
+    def test3(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.left.left = TreeNode(3)
+        root.left.left.left = TreeNode(4)
+        self.assertEqual(sum_of_depths(root), 6)
+
+    def test4(self):
+        self.assertEqual(sum_of_depths(None), 0)
